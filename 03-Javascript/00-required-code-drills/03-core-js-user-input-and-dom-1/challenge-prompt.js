0fc79f232a4900a62d8e163e5c07af5a230cf380
Sam Thompson <electronic62794@yahoo.com>
2019-02-23 18:21:02 -0800

    added javascript code drills and did drills 1-7

--- 03-Javascript/00-required-code-drills/03-core-js-user-input-and-dom-1/challenge-prompt.js
+++ 03-Javascript/00-required-code-drills/03-core-js-user-input-and-dom-1/challenge-prompt.js
@@ -3,11 +3,14 @@
 
 
 // Declare a variable `userName`
-
+var userName;
 
 // Prompt the user for their name and store their response into `userName`
 // hint: use javascript's built in `prompt` method
+userName = prompt("enter your name");
 
+
+console.log("welcome to the sandwich shop " + userName);
 
 
 // Create an alert welcoming the user to the "Sandwich Shop" using the name they just gave us
@@ -16,14 +19,18 @@
 
 // Using the `confirm` method, ask the user if they would like a sandwich
 // Store their response into a new `wantsSandwich` variable
-
+var wantsSandwich = confirm("would you like a sandwich?");
 
 
 // Check if the user wanted a sandwich
-
+if(wantsSandwich === true){
+    document.write("Your sandwich will be ready soon!");
+}
 
     // If the user did want a sandwich, use the `document.write` method to write "Your sandwich will be ready soon!" to the DOM
-
+else{
+    document.write("Goodbye" + userName);
+}
     
 // Else, say goodbye to the user using the name they gave us earlier
 
--- 03-Javascript/00-required-code-drills/04-core-js-user-input-and-dom-2/challenge-prompt.js
+++ 03-Javascript/00-required-code-drills/04-core-js-user-input-and-dom-2/challenge-prompt.js
@@ -4,6 +4,16 @@
 
 // Using the `confirm` method, welcome the user to the Boba Shop and ask them if they would like a boba
 // Store the user response in a variable `wantsBoba`
+var wantsBoba = confirm("welcome to the Boba shop! would you like a boba?");
+
+if(wantsBoba === true){
+    var drinkFlavor = prompt("Which flavor would you like?")
+    document.write("Alright, that'll be 22.50, your " + drinkFlavor + " Boba, will be out soon");
+}
+
+else{
+    document.writre("Goodbye! come back soon");
+}
 
 
 // Create an if statement checking whether the user wanted boba from the earlier `confirm` dialogue
